114cdb6c25669021add8b73cb1fd7b974efbf3f6
jeff <jeff@giftbit.com>
2021-10-06 20:58:17 -0700

    Remove thread worker code.  The speed up was marginal because encryption and description is dwarfed by insertion time and it was harder to make correct.  There was a problem where hash secret was not being passed in correctly.

--- src/decrypt/decryptRothschild.ts
+++ src/decrypt/decryptRothschild.ts
@@ -1,45 +1,19 @@
-import workerThreads = require("worker_threads");
 import {listFiles, readFile, writeFile} from "../utils/fileUtils";
 import {decryptCode} from "../utils/rothschildCrypto";
-import {groupArray} from "../utils/groupArray";
-import {workerPromise} from "../utils/workerPromise";
-
-const threadCount = 4;
 
 export async function decryptRothschild(accountId: string, encryptionSecret: string): Promise<void> {
     console.log("Decrypting Rothschild");
 
     const filenames = await listFiles(accountId, "download", "rothschild-Values");
-    if (threadCount < 2) {
-        await decryptRothschildFilesSerial(accountId, encryptionSecret, filenames);
-    } else {
-        await decryptRothschildFilesParallel(accountId, encryptionSecret, filenames);
-    }
-}
-
-async function decryptRothschildFilesSerial(accountId: string, encryptionSecret: string, filenames: string[]): Promise<void> {
     for (const filename of filenames) {
         await decryptRothschildFile(accountId, encryptionSecret, filename);
     }
 }
 
-async function decryptRothschildFilesParallel(accountId: string, encryptionSecret: string, filenames: string[]): Promise<void> {
-    for (const filenameGroup of groupArray(filenames, threadCount)) {
-        await Promise.all(filenameGroup
-            .map(filename =>
-                new workerThreads.Worker(
-                    __dirname + "/decryptRothschildWorker.js",
-                    {
-                        workerData: {accountId,encryptionSecret, filename}
-                    }
-                )
-            )
-            .map(workerPromise)
-        );
-    }
-}
-
 export async function decryptRothschildFile(accountId: string, encryptionSecret: string, filename: string): Promise<void> {
+    if (!encryptionSecret) {
+        throw new Error("encryptionSecret is nullish");
+    }
     const values: any[] = await readFile(accountId, "download", filename);
     for (const value of values) {
         if (value.codeEncrypted) {
--- src/encrypt/encryptRothschild.ts
+++ src/encrypt/encryptRothschild.ts
@@ -1,48 +1,25 @@
-import workerThreads = require("worker_threads");
 import {listFiles, readFile, writeFile} from "../utils/fileUtils";
 import {computeCodeLookupHash, encryptCode} from "../utils/rothschildCrypto";
-import {groupArray} from "../utils/groupArray";
-import {workerPromise} from "../utils/workerPromise";
-
-const threadCount = 4;
 
 export async function encryptRothschild(accountId: string, encryptionSecret: string, lookupHashSecret: string): Promise<void> {
     console.log("Encrypting Rothschild");
 
     const filenames = await listFiles(accountId, "decrypt", "rothschild-Values");
-    if (threadCount < 2) {
-        await encryptRothschildFilesSerial(accountId, encryptionSecret, lookupHashSecret, filenames);
-    } else {
-        await encryptRoschildFilesParallel(accountId, encryptionSecret, lookupHashSecret, filenames);
-    }
-}
-
-async function encryptRothschildFilesSerial(accountId: string, encryptionSecret: string, lookupHashSecret: string, filenames: string[]): Promise<void> {
     for (const filename of filenames) {
         await encryptRothschildFile(accountId, encryptionSecret, lookupHashSecret, filename);
     }
 }
 
-async function encryptRoschildFilesParallel(accountId: string, encryptionSecret: string, lookupHashSecret: string, filenames: string[]): Promise<void> {
-    for (const filenameGroup of groupArray(filenames, threadCount)) {
-        await Promise.all(filenameGroup
-            .map(filename =>
-                new workerThreads.Worker(
-                    __dirname + "/encryptRothschildWorker.js",
-                    {
-                        workerData: {accountId,encryptionSecret, filename}
-                    }
-                )
-            )
-            .map(workerPromise)
-        );
-    }
-}
-
 export async function encryptRothschildFile(accountId: string, encryptionSecret: string, lookupHashSecret: string, filename: string): Promise<void> {
+    if (!encryptionSecret) {
+        throw new Error("encryptionSecret is nullish");
+    }
+    if (!lookupHashSecret) {
+        throw new Error("lookupHashSecret is nullish");
+    }
     const values: any[] = await readFile(accountId, "decrypt", filename);
     for (const value of values) {
-        if (!value.userId.startsWith("user-")) {
+        if (!value?.userId?.startsWith("user-")) {
             throw new Error(`Value ${value.id} does not have a proper userId.`);
         }
         if (value.codeDecrypted) {
